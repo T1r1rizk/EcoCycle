92247f2b1d8943bd72bb87e191e1a29db3533a99
Tony <sideresearch.acc@gmail.com>
2025-05-28 02:09:53 +0300

    Update supabase_init.dart

--- lib/supabase_init.dart
+++ lib/supabase_init.dart
@@ -3,8 +3,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseInit {
   static Future<void> initializeSupabase() async {
     await Supabase.initialize(
-      url: 'https://kjwlbecbenkclllegqqp.supabase.co',  // Your Supabase project URL
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtqd2xiZWNiZW5rY2xsbGVncXFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNjY4MzEsImV4cCI6MjA1OTY0MjgzMX0.gfdIwJ4DMQQEttqQa84v0FsAGoNcBNQpUU5nWiS8WZw',  // Your Supabase anon/public key
+      url: 'your url',  // Your project URL
+      anonKey: 'your key',  // Your anon/public key
       debug: true,  // Only for development
     );
   }
